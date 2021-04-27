e6219e8911771040e500a36258d372aceb5f942d
JW-Schuetz <jws1954@gmx.de>
2021-04-27 11:35:24 +0200

    Aktualisierung

---
+++ DotNet/PreProcess/PreProcess.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+
+namespace PreProcess
+{
+    public class PreProcess
+    {
+        public static int OpenFile( string fileName )
+        {
+            if ( !File.Exists( fileName ) ) return -1;
+
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                using (StreamReader sr = File.OpenText(fileName))
+                {
+                    string s;
+                    while ((s=sr.ReadLine()) != null)
+                    {
+                        var split = s.Split(',');
+                        byte[] a = System.Text.Encoding.UTF8.GetBytes(s);
+                        byte[] hash = sha256.ComputeHash(a);
+                        char[] b = System.Text.Encoding.UTF8.GetChars(hash);
+                        string c = b.ToString();
+                    }
+
+                    return 0;
+                }
+            }
+        }
+    }
+}
