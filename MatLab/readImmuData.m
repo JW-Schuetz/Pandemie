24a7ed665f2589c04254fb60b3c3f08b45676480
JW-Schuetz <jws1954@gmx.de>
2021-06-11 10:12:46 +0200

    Aktualisierung

--- MatLab/readImmuData.m
+++ MatLab/readImmuData.m
@@ -1,8 +1,8 @@
 function datenstand = readImmuData( infile )
     % Sheet 4 auswerten
     sheet4 = 4;
-    names  = { 'Dat',      'Erst',  'Zweit', 'Gesamt' };
-    types  = { 'datetime', 'int32', 'int32', 'int32'  };
+    names  = { 'Dat',  'Erst',  'Zweit', 'Gesamt' };
+    types  = { 'char', 'int32', 'int32', 'int32'  };
     opts   = spreadsheetImportOptions( 'Sheet', sheet4, 'NumVariables', length( names ), ...
                 'VariableNames', names, 'VariableTypes', types, 'DataRange', 'A2' );
     tabSheet3 = readtable( infile, opts );
@@ -14,6 +14,7 @@ function datenstand = readImmuData( infile )
     gesamt = tabSheet3{ :, 'Gesamt' };
 
     % Index der gültigen
+    dat = datetime( dat );
 	ndx = ~isnat( dat );
 
     % die gültigen Datensätze sind
